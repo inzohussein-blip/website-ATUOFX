3545f97f0a2918cd09ef04c9658f5b2a0f4e52d4
inzohussein-blip <inzohussein@gmail.com>
2026-04-03 19:42:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // هذا السطر سيجعل الموقع يفتح حتى لو وجد أخطاء برمجية بسيطة
     ignoreBuildErrors: true,
   },
   eslint: {
+    // وهذا السطر سيتجاهل تحذيرات التنسيق أثناء الرفع
     ignoreDuringBuilds: true,
   },
 };
